3e16598984e77f7e789035c3c769ca932c324578
obul63 <obulgiggles@gmail.com>
2024-08-29 13:04:39 +0530

    Add MongoDB integration and update styling for header button and pagination

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./config/db')
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to db
+connectDB();
 
 app.use(express.static('public'));
 
